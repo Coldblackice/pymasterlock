138b7161cc4d5a2dcab879c05fdbd13a59a60d56
STumbles <imstumbles@gmail.com>
2015-04-28 16:37:36 -0700

    Fixed simple module typo

--- PyMasterLock.py
+++ PyMasterLock.py
@@ -1,5 +1,5 @@
 import argparse
-import iteritems
+import itertools
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--first", help="""Find the "First Locked Position"Rotate dial to the left (towards 10) hard until the dial gets locked.Notice how the dial is locked into a small groove. If you're directly between two digits such as 3 and 4, release the shackle and turn the dial left further until you're into the next locked groove. However, if the dial is between two half digits (e.g., 2.5 and 3.5), then enter the digit in-between (e.g., 3) into First Locked Position in the calculator below.""", type=float)
