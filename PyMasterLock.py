62c52efba2ed0a6b6999461dc90289ccd059b1cf
STumbles <imstumbles@gmail.com>
2015-04-28 16:35:07 -0700

    Increased readability

--- PyMasterLock.py
+++ PyMasterLock.py
@@ -1,4 +1,5 @@
 import argparse
+import iteritems
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--first", help="""Find the "First Locked Position"Rotate dial to the left (towards 10) hard until the dial gets locked.Notice how the dial is locked into a small groove. If you're directly between two digits such as 3 and 4, release the shackle and turn the dial left further until you're into the next locked groove. However, if the dial is between two half digits (e.g., 2.5 and 3.5), then enter the digit in-between (e.g., 3) into First Locked Position in the calculator below.""", type=float)
@@ -31,10 +32,8 @@ def combo(x):
 			second.append(float(temp))
 
 	print "[*] First Digit is :" + str(first)
-
-	for i in second:
-		print "[*] Second Digit is : %i\r" % i
-
+	print "[*] Second digits could be: + ', '.join(itertools.imap(str, second))
+	print "[*] third digits could be: + ', '.join(itertools.imap(str, third))
 	for i in third:
 		print "[*] Third Digit is : %i\r" % i
 
