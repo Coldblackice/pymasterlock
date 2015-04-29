db73f226ab219ec85187f111f1fbe8e77f86cd0c
STumbles <imstumbles@gmail.com>
2015-04-29 02:24:44 -0700

    Increased readability

--- PyMasterLockClass.py
+++ PyMasterLockClass.py
@@ -15,11 +15,11 @@ class MasterLock():
 		self.second = []
 		self.third = []
 		
-		self.l1 = float(sys.argv[1])
-		self.l2 = float(sys.argv[2])
-		self.r1 = float(sys.argv[3])
+		self.l1 = float( sys.argv[1] )
+		self.l2 = float( sys.argv[2] )
+		self.r1 = float( sys.argv[3] )
 
-		self.first = (self.r1 + 5) % 40
+		self.first = ( self.r1 + 5 ) % 40
 		self.mod = self.first % 4
 
 	def get_first(self):
@@ -33,7 +33,7 @@ class MasterLock():
 		(e.g., 2.5 and 3.5),then enter the digit in-between
 		(e.g., 3) into First Locked Position in the calculator below."""
 
-		return float((self.r1 + 5) % 40)
+		return float( ( self.r1 + 5 ) % 40 )
 
 	def get_second(self):
 		"""Find the "Second Locked Position"Do 
@@ -43,10 +43,10 @@ class MasterLock():
 		the whole number (e.g., 7) into Second 
 		Locked Position in the calculator below."""
 		for i in xrange(10):
-			temp = ((self.mod + 2) % 4) + (4 * i)
-			if ( not self.x or ( (third[self.x-1] +2) %40 != temp and (third[self.x-1] -2) %40) ): self.second.append(float(temp))
+			temp = ( ( self.mod + 2 ) % 4 ) + ( 4 * i )
+			if ( not self.x or ( ( third[self.x-1] +2 ) %40 != temp and ( third[self.x-1] -2 ) %40 ) ): self.second.append( float( temp ) )
 				
-		return ', '.join(itertools.imap(str, self.second))
+		return ', '.join( itertools.imap( str, self.second ) )
 
 	def get_third(self):
 		"""Find the "Resistant Location"Apply 
@@ -60,10 +60,10 @@ class MasterLock():
 		into Resistant Location. If the resistance 
 		begins at a half number, such as 14.5, enter 14.5."""
 		for i in xrange(4):
-			if (((10 * i) + self.l1) % 4) == self.mod: self.third.append(float((10 * i) + self.l1))
-			if (((10 * i) + self.l2) % 4) == self.mod: self.third.append(float((10 * i) + self.l2))
+			if ( ( ( 10 * i ) + self.l1 ) % 4 ) == self.mod: self.third.append( float( (10 * i ) + self.l1 ) )
+			if ( ( ( 10 * i ) + self.l2 ) % 4 ) == self.mod: self.third.append( float( (10 * i ) + self.l2 ) )
 				
-		return ', '.join(itertools.imap(str, self.third))
+		return ', '.join( itertools.imap( str, self.third ) )
 
 	if len(sys.argv) != 4:
 		sys.exit('Usage: %s firstnumber secondnumber thirdnumber' % sys.argv[0] + '\n' + "Example: python2 %s 3 6 10" % sys.argv[0])
